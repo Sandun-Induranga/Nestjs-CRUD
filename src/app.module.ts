e86e8bb4235da3cfd494571d4f3a080d624db814
Sandun Induranga <sandutharu40@gmail.com>
2023-06-27 20:52:24 +0530

    Customer Controller Created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersController } from './customers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CustomersController],
   providers: [AppService],
 })
 export class AppModule {}
