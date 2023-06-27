5bfd41b421afb52ddb7f05bdfc0710063efcdf5a
Sandun Induranga <sandutharu40@gmail.com>
2023-06-27 21:19:43 +0530

    Function saveCustoemr() Created

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  saveCustomer(): string {
+    return 'Save Customer Invoked';
+  }
+}
