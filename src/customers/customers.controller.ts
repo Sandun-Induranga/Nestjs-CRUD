2d7e942c9e859ed03abb46d10d4a17d5e1d8d577
Sandun Induranga <sandutharu40@gmail.com>
2023-06-27 21:00:07 +0530

    Function findAll() Created

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all customers';
+  }
+}
