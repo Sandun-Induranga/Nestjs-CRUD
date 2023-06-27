2ddeefee54e9dd65ddb9f9c3cd22442ed35b0c66
Sandun Induranga <sandutharu40@gmail.com>
2023-06-27 22:08:45 +0530

    Function getAllCustomers() Refactored

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Customer } from './interfaces/customer';
 
 @Injectable()
 export class CustomersService {
-  async saveCustomer(): Promise<string> {
-    return 'Save Customer Invoked';
+  constructor(
+    @InjectModel('Customer') private readonly customerModel: Model<Customer>,
+  ) {}
+
+  async getAllCustomer(): Promise<Customer[]> {
+    return await this.customerModel.find();
   }
 }
