def5ebe4adfe3e0087ee00df794d8a63bdf7adff
Sandun Induranga <sandutharu40@gmail.com>
2023-06-28 11:23:19 +0530

    Crated Function createCustomerDto()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomersController } from './customers/customers.controller';
-import { CustomersService } from './customers/customers.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomersModule } from './customers/customers.module';
 import { keys } from './config/keys';
--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
+
+  @Post()
+  async create(
+    @Body() createCustomerDto: CreateCustomerDto,
+  ): Promise<Customer> {
+    return this.customersService.saveCustomer();
+  }
 
   @Get()
   async findAll(): Promise<Customer[]> {
